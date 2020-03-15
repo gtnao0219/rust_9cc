81ad432276c5975a4173b782ba01e05a3fe0ef44
naotaka nakane <n.nakane0219@gmail.com>
2020-03-15 18:26:50 +0900

    step2 except abnormal cases

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,37 @@ fn main() {
         eprintln!("{}: invalid number of arguments", args[0]);
         process::exit(1);
     }
+    let chars = args[1].chars();
     println!(".intel_syntax noprefix");
     println!(".global main");
     println!("main:");
-    println!("  mov rax, {}", args[1].parse::<i32>().unwrap());
+    
+    let mut n = 0;
+    let mut op = '.';
+    for c in chars {
+        if c.is_digit(10) {
+            n = n * 10 + c.to_digit(10).unwrap();
+        } else {
+            if op == '+' {
+                println!("  add rax, {}", n);
+            } else if op == '-' {
+                println!("  sub rax, {}", n);
+            } else {
+                println!("  mov rax, {}", n);
+            }
+            n = 0;
+            op = c;
+        }
+        // eprintln!("unexpected character: '{}'", c);
+        // process::exit(1);
+    }
+    if op == '+' {
+        println!("  add rax, {}", n);
+    } else if op == '-' {
+        println!("  sub rax, {}", n);
+    } else {
+        println!("  mov rax, {}", n);
+    }
+
     println!("  ret");
 }
